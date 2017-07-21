b6ea31c89c26a3a5dba281b83221e2dee0841086
AxoftDesarrollo <Backoffice@axoft.com>
2017-07-21 14:50:35 -0300

    Initial commit

---
+++ AperturaRestoService/Api/FacturaController.cs
@@ -0,0 +1,47 @@
+﻿using AperturaRestoService.Dto;
+using System;
+using System.Web.Http;
+
+namespace AperturaRestoService.Api
+{
+    public class FacturaController : BaseController
+    {
+        private void LogData(FacturaDto data)
+        {
+            Log.Info("Detalle de la factura");
+            Log.Info(data.ComandaId);
+            Log.Info(data.Fecha);
+            Log.Info(data.Hora);
+            Log.Info(data.Estado);
+            Log.Info(data.Total);
+            Log.Info("Detalle de renglones");
+            if (data.Detalle != null)
+            {
+                foreach (var renglon in data.Detalle)
+                {
+                    string line = $"{renglon.NumeroRenglon};{renglon.CodigoArticulo}; ;{renglon.Cantidad}; ;{renglon.Importe}";
+                    Log.Info(line);
+                }
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult AfterInsert(FacturaDto data)
+        {
+            try
+            {
+                Log.Debug("FacturaController.AfterInsert: Inicio");
+                // Desarrollo de lógica de apertura
+                LogData(data);
+                // Fin de lógica de apertura
+                Log.Debug("FacturaController.AfterInsert: Fin");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal("FacturaController.AfterInsert", e);
+                return InternalServerError();
+            };
+        }
+    }
+}
